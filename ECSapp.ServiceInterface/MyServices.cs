0ed6f557a4046c625c1683d0dfa2ca438b300ebd
teejoshua <teejoshua01@gmail.com>
2022-07-26 17:20:18 +0100

    first commit

---
+++ ECSapp.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using ECSapp.ServiceModel;
+
+namespace ECSapp.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
---
+++ ECSapp/Configure.AppHost.cs
@@ -0,0 +1,25 @@
+using Funq;
+using ServiceStack;
+using ECSapp.ServiceInterface;
+
+[assembly: HostingStartup(typeof(ECSapp.AppHost))]
+
+namespace ECSapp;
+
+public class AppHost : AppHostBase, IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+        });
+
+    public AppHost() : base("ECSapp", typeof(MyServices).Assembly) {}
+
+    public override void Configure(Container container)
+    {
+        // Configure ServiceStack only IOC, Config & Plugins
+        SetConfig(new HostConfig {
+            UseSameSiteCookies = true,
+        });
+    }
+}
